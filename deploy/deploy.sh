6139a5c6cd223b5c958f577cf001ce8c73f4f7af
Cheikh Ba <cheikh.evry@gmail.com>
2024-12-28 22:35:49 +0000

    add comment and refacto

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,12 @@
-
-
-docker image pull cheikhckrt/custom-laravel:8.2
-
-microk8s kubectl apply -f nginx-config.conf
-
+#pull custom-laravel image (deploy/Dockerfile)
+docker image pull cheikhckrt/custom-laravel:8.3
+#apply deployment files
+microk8s kubectl apply -f nginx-config.yaml
 microk8s kubectl apply -f laravel-config-env.yaml
-
 microk8s kubectl apply -f laravel-setup.yaml
 
+#add repo bitnami
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-
+#install mongodb-sharded
 helm install mongodb-sharded bitnami/mongodb-sharded   --namespace dev   --create-namespace   --values mongo-shared/values.yaml
